acfea8fb307ceb77f91ce535d5b19d59617e0eb6
Zia Hellman <ziahellman@gmail.com>
2022-05-31 16:20:48 +0300

    commit tree d22b02e1c109525b3c20d89e8da83dddab2e026e

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+
+const names = [["Charlie", "Samip", "Ali"]];
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`thank you ${names}`);
+  }
+}
